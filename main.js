1c8fc69ca4c7a56c3b798d13d118680be28e18c2
shahnapinit <shahana.p@initsolutions.in>
2024-08-07 18:28:25 +0530

    dvdcxv xc

--- main.js
+++ main.js
@@ -311,6 +311,7 @@ app.get('/master/main/getexpensebydate', async (req, res) => {
   });
   
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
